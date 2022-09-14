4a819b7cd712579d8179238d4fac03b1e5783a12
AlejoMejia96 <alejandroquinteromejia@gmail.com>
2022-09-14 18:36:04 -0500

    nuevo repo

---
+++ 01-JS-I/homework/demo.js
@@ -0,0 +1,34 @@
+var usuario = "alejo_123";
+
+var edad = 25;
+
+var NumeroTelefono = 3000232354;
+
+var _pais = "Colombia";
+
+var emailUsuario = 'mail@mail.com';  
+
+var Hola123 = 34;
+
+var enlace = 'https://www.google.com';
+
+var MiEmail = 'miemail@mail.com'
+
+var miEmailAntiguo = '...';  
+
+//--------------------------------------------------
+
+var laReinaEstaViva = false;
+
+//tiene licencia?
+
+var licencia = false;
+
+var cadena = 'McDonald\'s';
+
+var hola;
+
+var vasoDeAgua;
+
+console.log("hola como estas")
+
